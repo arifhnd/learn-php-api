c989e2a44b892c7eddc2f5e4b45057292f44dd51
root <forevertkj4@gmail.com>
2019-02-05 17:15:03 +0700

    initial commit

---
+++ latihan1.php
@@ -0,0 +1,18 @@
+<?php 
+$someArray = [
+    [
+      "name"   => "Jonathan Suh",
+      "gender" => "male"
+    ],
+    [
+      "name"   => "William Philbin",
+      "gender" => "male"
+    ],
+    [
+      "name"   => "Allison McKinnery",
+      "gender" => "female"
+    ]
+  ];
+
+echo json_encode($someArray);
+?>
